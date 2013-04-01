d53833e87e08f6b17e03f96866691691e7705510
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-04-01 19:01:21 +0300

    Working commit

--- test.c
+++ test.c
@@ -1,14 +1,22 @@
-#include <math.h>
+#include <stdio.h>
 #include <geometry3d.h>
 
 int main() {
-	Object3d * t = new_triangle(
-                                point3d(0, 0, 0),
-                                point3d(1, 0, 0),
-                                point3d(0, 1, 0),
-                                rgb(100, 255, 0));
+    Scene * scene = new_scene(1);
     
-	t->rotate(t->data, 0, M_PI / 3);
-	t->print(t->data);
+    scene->objects[0] = new_triangle(
+                                     point3d(0, 0, 0),
+                                     point3d(10, 0, 0),
+                                     point3d(0, 10, 0),
+                                     rgb(100, 255, 0));
+    
+    rotate_scene(scene, 0, 0);
+    
+    Color col;
+    trace(scene, point3d(1, 1, 1), vector3df(0, 0, -1), &col);
+    
+    printf("r = %i, g = %i, b = %i\n", col.r, col.g, col.b);
+    
+    release_scene(scene);
 	return 0;
 }
