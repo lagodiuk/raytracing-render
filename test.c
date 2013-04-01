dac104ce043f25e6e46ad0a8a064417027981d5f
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-04-02 01:12:01 +0300

    First draft test of ray tracing :))

--- test.c
+++ test.c
@@ -4,27 +4,33 @@
 #include <geometry3d.h>
 
 int main() {
-    Scene * scene = new_scene(3);
+    Scene * scene = new_scene(4);
     
     scene->objects[0] = new_triangle(
-                                     point3d( 0,  0, 0),
-                                     point3d(10,  0, 0),
-                                     point3d( 0, 10, 0),
-                                     rgb(100, 255, 0));
+                                     point3d( 0,   0, 80),
+                                     point3d(100,  0, 80),
+                                     point3d( 0, 100, 80),
+                                     rgb(255, 55, 0));
     
     scene->objects[1] = new_triangle(
-                                     point3d( 0,  0, 20),
-                                     point3d(10,  0, 20),
-                                     point3d( 0, 10, 20),
-                                     rgb(0, 255, 255));
+                                     point3d(100, 100, 80),
+                                     point3d(100,  0, 80),
+                                     point3d( 0, 100, 80),
+                                     rgb(255, 55, 0));
     
     scene->objects[2] = new_triangle(
-                                     point3d( 0,  0, 10),
-                                     point3d(10,  0, 10),
-                                     point3d( 0, 10, 10),
-                                     rgb(0, 255, 0));
+                                     point3d( 0,   0, 80),
+                                     point3d(100,  0, 80),
+                                     point3d(100,  0, 180),
+                                     rgb(55, 255, 0));
     
-    rotate_scene(scene, M_PI / 4, M_PI / 2);
+    scene->objects[3] = new_triangle(
+                                     point3d( 0,   0, 80),
+                                     point3d( 0,   0, 180),
+                                     point3d(100,  0, 180),
+                                     rgb(55, 255, 0));
+    
+    rotate_scene(scene, M_PI / 3, M_PI / 2);
     
     int i;
     int j;
@@ -34,7 +40,7 @@ int main() {
     
     for(i = -100; i < 101; i++) {
         for(j = -100; j < 101; j++) {
-            trace(scene, point3d(0, 0, -10), vector3df(i, j, -5), &col);
+            trace(scene, point3d(-20, -20, -140), vector3df(i, j, 50), &col);
             
             set_pixel(i+100, j+100, col, canv);
         }
