44e08e159e02a81984d873d205d221c7347876d2
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-04-03 02:33:20 +0300

    Refactoring

--- render/headers/utils.h
+++ render/headers/utils.h
@@ -5,14 +5,14 @@
 
 #define EPSILON 1e-5
 
-inline Float cos_vectors3d(Vector3d v1, Vector3d v2);
+inline Float cos_vectors(Vector3d v1, Vector3d v2);
 
-inline Float module_vector3d(Vector3d v);
+inline Float module_vector(Vector3d v);
 
 inline void normalize_vector(Vector3d * v);
 
 inline Float herons_square(Float a, Float b, Float c);
 
-inline Point3d rotate(Point3d p, Float sin_al, Float cos_al, Float sin_be, Float cos_be);
+inline Point3d rotate_point(Point3d p, Float sin_al, Float cos_al, Float sin_be, Float cos_be);
 
 #endif
