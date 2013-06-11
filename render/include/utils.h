c88001fefc94893cd6be25a3389f97a4f9073b5a
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-06-11 18:33:44 +0300

    Refactoring of utils

--- render/include/utils.h
+++ render/include/utils.h
@@ -4,6 +4,22 @@
 #include <render.h>
 #include <math.h>
 
+static inline Vector3d
+cross_product(const Vector3d a,
+              const Vector3d b) {
+    
+    return vector3df(a.z * b.y - a.y * b.z,
+                     a.x * b.z - a.z * b.x,
+                     a.y * b.x - a.x * b.y);
+}
+
+static inline Float
+dot_product(const Vector3d v1,
+            const Vector3d v2) {
+    
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
 static inline Float
 module_vector(const Vector3d v) {
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
@@ -18,17 +34,28 @@ static inline Float
 cos_vectors(const Vector3d v1,
             const Vector3d v2) {
     
-    Float numerator = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-    Float denominator = module_vector(v1) * module_vector(v2);
-    return numerator / denominator;
+    return dot_product(v1, v2) / sqrt(sqr_module_vector(v1) * sqr_module_vector(v2));
 }
 
 static inline void
 normalize_vector(Vector3d * const v) {
-    Float module = module_vector(*v);
+    const Float module = module_vector(*v);
     v->x = v->x / module;
     v->y = v->y / module;
     v->z = v->z / module;
+}
+
+static inline Vector3d
+reflect_ray(const Vector3d incident_ray,
+            const Vector3d norm_v) {
+    
+    const Float k = 2 * dot_product(incident_ray, norm_v) / sqr_module_vector(norm_v);
+    
+    const Float x = incident_ray.x - norm_v.x * k;
+    const Float y = incident_ray.y - norm_v.y * k;
+    const Float z = incident_ray.z - norm_v.z * k;
+    
+    return vector3df(x, y, z);
 }
 
 // TODO: use matrixes
@@ -97,41 +124,5 @@ rotate_vector_z(const Vector3d p,
 	const Float y = p.x * sin_al + p.y * cos_al;
     
 	return vector3df(x, y, p.z);
-}
-
-static inline Vector3d
-reflect_ray(const Vector3d incident_ray,
-            const Vector3d norm_v) {
-    
-    Float numerator = 2 * (incident_ray.x * norm_v.x
-                           + incident_ray.y * norm_v.y
-                           + incident_ray.z * norm_v.z);
-    
-    Float norm_module = module_vector(norm_v);
-    Float denominator = norm_module * norm_module;
-    
-    Float k = numerator / denominator;
-    
-    Float x = incident_ray.x - norm_v.x * k;
-    Float y = incident_ray.y - norm_v.y * k;
-    Float z = incident_ray.z - norm_v.z * k;
-    
-    return vector3df(x, y, z);
-}
-
-static inline Vector3d
-cross_product(const Vector3d a,
-              const Vector3d b) {
-    
-    return vector3df(a.z * b.y - a.y * b.z,
-                     a.x * b.z - a.z * b.x,
-                     a.y * b.x - a.x * b.y);
-}
-
-static inline Float
-dot_product(const Vector3d v1,
-            const Vector3d v2) {
-    
-    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 #endif
