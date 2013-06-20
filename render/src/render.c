04ef3ad9ac62d9d171d0483d934143ca05c77d05
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-06-21 01:05:03 +0300

    Improving render core (better integration with OpenMP)

--- render/src/render.c
+++ render/src/render.c
@@ -7,118 +7,33 @@
 
 #include <omp.h>
 
-typedef
-struct {
-    Scene * scene;
-    Camera * camera;
-    Canvas * canvas;
-    
-    Float dx;
-    Float dy;
-    
-    int x_min;
-    int x_max;
-    int y_min;
-    int y_max;
-}
-RenderSceneData;
-
-inline void
-render_part_of_scene(const RenderSceneData * const data);
-
-inline RenderSceneData *
-new_render_scene_data(Scene * const scene,
-                      Camera * const camera,
-                      Canvas * const canvas);
-
 void
 render_scene(Scene * scene,
              Camera * const camera,
              Canvas * canvas,
              int num_threads) {
     
-    if(num_threads < 2) {
-        RenderSceneData * data =
-            new_render_scene_data(scene, camera, canvas);
-        
-        render_part_of_scene(data);
-        
-        free(data);
-        return;
-    }
-
-    int slice_width = canvas->w / num_threads;
-    
-    omp_set_num_threads(num_threads);
-    #pragma omp parallel
-    {
-        int i = omp_get_thread_num();
-        
-        RenderSceneData * data = new_render_scene_data(scene, camera, canvas);
-        data->x_min = slice_width * i;
-        data->x_max = slice_width * (i + 1);
-        
-        if(i == num_threads - 1)
-            data->x_max = canvas->w;
-        
-        render_part_of_scene(data);
-        
-        free(data);
-    }
-}
-
-inline RenderSceneData *
-new_render_scene_data(Scene * const scene,
-                      Camera * const camera,
-                      Canvas * const canvas) {
-    
-    RenderSceneData * data = malloc(sizeof(RenderSceneData));
-    data->scene = scene;
-    data->camera = camera;
-    data->canvas = canvas;
-    data->dx = canvas->w / 2.0;
-    data->dy = canvas->h / 2.0;
-    data->x_min = 0;
-    data->x_max = canvas->w;
-    data->y_min = 0;
-    data->y_max = canvas->h;
-    
-    return data;
-}
-
-inline void
-render_part_of_scene(const RenderSceneData * const data) {
-    
-    const Float dx = data->dx;
-    const Float dy = data->dy;
-    
-    Scene * const scene = data->scene;
-    Camera * const camera = data->camera;
-    Canvas * canvas = data->canvas;
+    const int w = canvas->w;
+    const int h = canvas->h;
+    const Float dx = w / 2.0;
+    const Float dy = h / 2.0;
     const Float proj_plane_dist = camera->proj_plane_dist;
     
-    const int x_min = data->x_min;
-    const int x_max = data->x_max;
-    const int y_min = data->y_min;
-    const int y_max = data->y_max;
+    const int iter = w * h;
+    
+    num_threads = (num_threads < 2) ? 1 : num_threads;
+    omp_set_num_threads(num_threads);
     
     int i;
-    int j;
-    
-    Float x;
-    Float y;
-    
-    Color col;
-    
-    for(i = x_min; i < x_max; i++) {
-        for(j = y_min; j < y_max; j++) {
-            
-            x = i - dx;
-            y = j - dy;
-            
-            trace(scene, camera, vector3df(x, y, proj_plane_dist), &col);
-            
-            set_pixel(i, j, col, canvas);
-        }
+    #pragma omp parallel private(i)
+    #pragma omp for schedule(dynamic)
+    for(i = 0; i < iter; i++) {
+        const canv_x = i % w;
+        const canv_y = i / h;
+        const Float x = canv_x - dx;
+        const Float y = canv_y - dy;
+        Color col;
+        trace(scene, camera, vector3df(x, y, proj_plane_dist), &col);        
+        set_pixel(canv_x, canv_y, col, canvas);
     }
 }
